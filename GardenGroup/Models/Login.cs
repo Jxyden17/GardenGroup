08fe5d7e6706a063bf4fcc141fca116f3974e37a
Jxyden_ <ejsmeulemans@cs.com>
2025-10-13 12:17:00 +0200

    Fixes

---
+++ GardenGroup/Models/Login.cs
@@ -0,0 +1,18 @@
+﻿namespace GardenGroup.Models
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public Login()
+        {
+        }
+
+        public Login(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+    }
+}

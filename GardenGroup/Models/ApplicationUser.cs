e41aa292ff980a6c2789fa2801a2909aa5be665f
Jxyden17 <ejsmeulemans@cs.com>
2025-11-13 15:01:08 +0100

    Removed id from model

--- GardenGroup/Models/ApplicationUser.cs
+++ GardenGroup/Models/ApplicationUser.cs
@@ -5,13 +5,9 @@ namespace GardenGroup.Models
 {
     public class ApplicationUser : MongoUser
     {
-        // These extra fields are optional, but you can mirror your domain model.
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? City { get; set; }
 
-        [BsonElement("UserID")]
-        [BsonIgnoreIfNull]
-        public int? UserID { get; set; }
     }
 }
